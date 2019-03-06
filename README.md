f9a74668ac0e6cbe3697e7e05c65061262d3589e
Rahul Makhija <rahuls360@gmail.com>
2019-03-06 21:28:04 +0530

    bind <input> value to state

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Yet another Todo List app
 
 Build a traditional todo list application-> Add tasks, Edit tasks, Delete tasks. Persist data using localStorage.
+
+## Good to have features
+
+1. Use SCSS
--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    input: "",
+    todolist: []
+  };
+
+  handleChange = event => {
+    this.setState({ input: event.target.value });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>hi</h1>
+        <form action="">
+          <input
+            type="text"
+            value={this.state.input}
+            onChange={this.handleChange}
+          />
+        </form>
       </div>
     );
   }
