538a092fe62a1000cd42a252f8ef16a11c703841
Rahul Makhija <rahuls360@gmail.com>
2019-03-11 20:04:18 +0530

    onclicking task - toggles task completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,44 +1,64 @@
 import React from "react";
 
 class Todo extends React.Component {
-  state={
+  state = {
     editFlag: false,
-    task: this.props.task
-  }
+    task: this.props.task,
+    status: false
+  };
 
-  handleEdit= () => {
-  this.setState({editFlag: !this.state.editFlag});
-  }
+  handleEdit = () => {
+    this.setState({ editFlag: !this.state.editFlag });
+  };
 
   handleDelete = () => {
     this.props.deleteTask(this.props.index);
-  }
+  };
 
-  handleEditChange = (event) => {
-    this.setState({task: event.target.value});
-  }
+  handleEditChange = event => {
+    this.setState({ task: event.target.value });
+  };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     this.props.editTask(this.props.index, this.state.task);
-    this.setState({editFlag: false});
+    this.setState({ editFlag: false });
     event.preventDefault();
-  }
+  };
+
+  handleClick = event => {
+    this.setState({ status: !this.state.status });
+  };
 
   render() {
     let edit = <p>{this.state.task}</p>;
-    if(this.state.editFlag){
-      edit = <form onSubmit={this.handleSubmit} className="form-edit">
-        <input type="text" value={this.state.task} onChange={this.handleEditChange} autoFocus/>
-      </form>;
+    if (this.state.editFlag) {
+      edit = (
+        <form onSubmit={this.handleSubmit} className="form-edit">
+          <input
+            type="text"
+            value={this.state.task}
+            onChange={this.handleEditChange}
+            autoFocus
+          />
+        </form>
+      );
     }
     return (
       <div className="d-flex">
-        <div className="left">
+        <div
+          className={this.state.status ? "left done" : "left"}
+          onClick={this.handleClick}
+        >
           {edit}
         </div>
         <div className="right">
           <button onClick={this.handleEdit}>Edit</button>
-          <button onClick={this.handleDelete} style={{backgroundColor: "#e74c3c", marginLeft: '5px'}}>X</button>
+          <button
+            onClick={this.handleDelete}
+            style={{ backgroundColor: "#e74c3c", marginLeft: "5px" }}
+          >
+            X
+          </button>
         </div>
       </div>
     );
